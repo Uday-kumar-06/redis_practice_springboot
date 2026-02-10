d0c3f5781678fb15e68cae93cbc4096ccd35c772
Uday-kumar-06 <udaykumarkella67@gmail.com>
2026-02-10 20:31:32 +0530

    Add tthe Structure

---
+++ urlshortner/src/main/java/com/practice/urlshortner/controller/UrlController.java
@@ -0,0 +1,8 @@
+package com.practice.urlshortner.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UrlController {
+}
---
+++ urlshortner/src/main/java/com/practice/urlshortner/repository/UrlRepository.java
@@ -0,0 +1,7 @@
+package com.practice.urlshortner.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UrlRepository {
+}
